fb59b0b9a5ab7c75c44946a3be68e620bbe3ed93
Sam Stratton <38122515+taximadish@users.noreply.github.com>
2019-04-22 13:22:33 +1200

    name

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -18,24 +18,21 @@ namespace Common
 #define BUILD_TYPE_STR ""
 #endif
 
-const std::string scm_rev_str = "Dolphin "
-#if !SCM_IS_MASTER
-                                "[" SCM_BRANCH_STR "] "
-#endif
+const std::string scm_rev_str = "Taxi's Custom Dolphin"
 
 #ifdef __INTEL_COMPILER
     BUILD_TYPE_STR SCM_DESC_STR "-ICC";
 #else
-    BUILD_TYPE_STR SCM_DESC_STR;
+    BUILD_TYPE_STR "";
 #endif
 
 const std::string scm_rev_git_str = SCM_REV_STR;
-const std::string scm_desc_str = SCM_DESC_STR;
+const std::string scm_desc_str = "";
 const std::string scm_branch_str = SCM_BRANCH_STR;
 const std::string scm_distributor_str = SCM_DISTRIBUTOR_STR;
 
 #ifdef _WIN32
-const std::string netplay_dolphin_ver = SCM_DESC_STR " Win";
+const std::string netplay_dolphin_ver = "" " Win";
 #elif __APPLE__
 const std::string netplay_dolphin_ver = SCM_DESC_STR " Mac";
 #else
